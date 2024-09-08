4b932a52e00bc69849c677294961f0b7d34dec04
Stephen Torrence <captainvalor@gmail.com>
2024-09-08 16:45:18 -0700

    Dialing in...

--- src/App.tsx
+++ src/App.tsx
@@ -132,7 +132,6 @@ const App = () => {
           </div>
           <div className={`instructions ${instructionsVisible ? 'visible' : 'hidden'}`}>
             <p><strong>Click</strong> or <strong>Tap</strong></p>
-            <p>here to begin</p>
           </div>
         </div>
       </header>
